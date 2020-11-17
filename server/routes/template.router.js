327313bbabc0b30cc70ceb9545c994e85c0e9466
Emma Song Fisher <emmasongf@gmail.com>
2020-11-17 16:19:03 -0600

    GET sanatized

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -20,8 +20,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
 //where "wine"."user_id" =$1;  [req.user.id]
   
-  const queryText = `SELECT * FROM "wine" ;`;
-    pool.query(queryText)
+  const queryText = `SELECT * FROM "wine" WHERE "wine"."user_id" = $1 ;`;
+    pool.query(queryText, [req.user.id])
         .then( (result) => {
             res.send(result.rows);
         })
