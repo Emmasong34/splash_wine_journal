400ff7ed0df15f8c879c520502417ad6add641e5
Emma Song Fisher <emmasongf@gmail.com>
2020-11-18 15:08:16 -0600

    details for all wines displaying on details page

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -31,7 +31,7 @@ class UserPage extends Component {
         {/* {JSON.stringify(this.props.store.wineList)} */}
 
         {this.props.store.wineList.wineList.map((wine) => {
-          return <div className="wineDisplay">
+          return <div className="wineDisplay" key={wine.id}>
                     <p>winery: {wine.winery}, variety: {wine.variety}, 
                     name: {wine.name}, rating: {wine.rating}</p>
                     <button className="viewDetails" onClick={() => {this.handleClick(wine)}}>view wine</button>
--- src/components/ViewWine/ViewWine.js
+++ src/components/ViewWine/ViewWine.js
@@ -5,11 +5,38 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 
 class ViewWine extends Component {
 
+    componentDidMount() {
+        this.getWineList();
+      }
+
+      getWineList = () => {
+        this.props.dispatch({type: 'FETCH_WINE_LIST'});
+      }
+
     render(){
         return (
             <div>
-            {JSON.stringify(this.props.store.wineList)};
+            
+            
+            {/* {JSON.stringify(this.props.store.wineList)}; */}
+            
+
+                {this.props.store.wineList.wineList.map((wine) => {
+                    return <div className="viewWineDisplay" key={wine.id}>
+                        <h1>Details for {wine.name}</h1>
+                            <p>winery: {wine.winery}, variety: {wine.variety}, 
+                            name: {wine.name}, rating: {wine.rating}, 
+                            year: {wine.year}, price: {wine.price}, 
+                            fruity: {wine.fruity}, floral: {wine.floral}, 
+                            nutty: {wine.nutty}, earthy: {wine.earthy}, 
+                            herby: {wine.herby}, spicy: {wine.spicy}, 
+                            other: {wine.other}, overall: {wine.overall}, 
+                            notes: {wine.notes}</p>
+                            
+                        </div>
+                })}
             </div>
+
         )
     }
 }
