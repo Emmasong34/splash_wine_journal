eba9952d6664e4e08e73264562dac65ef47d1dc8
Emma Song Fisher <emmasongf@gmail.com>
2020-11-24 16:12:26 -0600

    bootstrap layout added to viewwine page

--- src/components/ViewWine/ViewWine.js
+++ src/components/ViewWine/ViewWine.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 class ViewWine extends Component {
@@ -38,15 +41,66 @@ class ViewWine extends Component {
                 {this.props.store.wineList.wineList.map((wine) => {
                     return <div className="viewWineDisplay" key={wine.id}>
                         <h1>Details for {wine.name}</h1>
-                            <p>winery: {wine.winery}, variety: {wine.variety}, 
-                            name: {wine.name}, rating: {wine.rating}, 
-                            year: {wine.year}, price: {wine.price}, 
-                            fruity: {wine.fruity}, floral: {wine.floral}, 
-                            nutty: {wine.nutty}, earthy: {wine.earthy}, 
-                            herby: {wine.herby}, spicy: {wine.spicy}, 
-                            other: {wine.other}, overall: {wine.overall}, 
-                            notes: {wine.notes}</p>
-                            
+                        <Container>
+                            <Row>
+                                <Col>
+                                    winery: {wine.winery} 
+                                </Col>
+                                <Col>
+                                    variety: {wine.variety}
+                                </Col>
+                              
+                            </Row>
+                            <Row>
+                                <Col>
+                                    name: {wine.name} 
+                                </Col>
+                                <Col>
+                                    rating: {wine.rating} 
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    year: {wine.year} 
+                                </Col>
+                                <Col>
+                                    price: {wine.price}
+                                </Col>
+                            </Row> 
+                            <Row>
+                                <Col>
+                                    fruity: {wine.fruity}
+                                </Col>
+                                <Col>
+                                    floral: {wine.floral} 
+                                </Col>
+                                <Col>
+                                    nutty: {wine.nutty} 
+                                </Col>
+                            </Row>
+                            <Row> 
+                                <Col>
+                                    earthy: {wine.earthy} 
+                                </Col>
+                                <Col>
+                                    herby: {wine.herby} 
+                                </Col>
+                                <Col>
+                                    spicy: {wine.spicy} 
+                                </Col>
+                            </Row>  
+                            <Row>
+                                <Col>
+                                    other: {wine.other} 
+                                </Col>
+                                <Col>
+                                    overall: {wine.overall} 
+                                </Col>
+                                <Col>
+                                    notes: {wine.notes}
+                                </Col>
+                            </Row>
+                        </Container>    
              
                 <button>home</button>               <button onClick={() => this.editWine(wine.id)}>edit</button> <button onClick={() => this.deleteWine(wine.id)}>delete</button> 
                 </div>
