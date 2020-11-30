ecb76950613d91a9d7de42bd2b113ffe9f107428
Emma Song Fisher <emmasongf@gmail.com>
2020-11-29 20:51:02 -0600

    about page edit

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -12,7 +12,7 @@ const AboutPage = () => (
       <p>My biggest challenge was deciding how to organize my data in the database tables</p>
       <p>Looking ahead, I would like to add a section where users can upload an image of the wine label 
         and an add friends feature to share wine lists</p>
-      <p>Big thank you to Casie and Kris, the Ukkonen cohort (especially Matt!), and Doctor Chips</p>
+      <p>Big thank you to Casie and Kris, the Ukkonen cohort, and Doctor Chips</p>
     </div>
   </div>
 );
