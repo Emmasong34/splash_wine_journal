ed5af57678c11882b38e5bc12196e1530e89c84e
Emma Song Fisher <emmasongf@gmail.com>
2021-02-24 14:09:21 -0600

    about section updated

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -4,12 +4,7 @@ import React from 'react';
 const AboutPage = () => (
   <div className="about">
     <div>
-      {/* this is a small about section for my Prime Digital Academy solo presentation */}
-      <p>Splash Wine Journal was created using React, Redux, PostgreSQL, and Bootstrap</p>
-      <p>My biggest challenge was deciding how to organize my data in the database tables</p>
-      <p>Looking ahead, I would like to add a section where users can upload an image of the wine label 
-        and an add friends feature to share wine lists</p>
-      <p>Big thank you to Casie and Kris, the Ukkonen cohort, and Doctor Chips</p>
+      <p>Splash Wine Journal is an alternative to pen and paper note taking for novice wine drinkers who wish to create profiles and enter specific details about a newly opened bottle of wine. Never forget your thoughts on a wine again!</p>
     </div>
   </div>
 );
